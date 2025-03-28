e0d3126402fe8a8125b9afa321215f443fe99581
Пачурина Дарья Викторовна <dvpachurin@mts.ru>
2025-03-28 16:33:07 +0300

    draft version of the graph generation and isomorphic graph construction algorithm has been added.

--- GIS_Data/src/Graph.cpp
+++ GIS_Data/src/Graph.cpp
@@ -25,4 +25,8 @@ namespace GIS_Data {
     int Graph::GetNodeCount() const {
         return nodeCount;
     }
+
+    GraphPair::GraphPair(const Graph& g1, const Graph& g2, bool flag)
+        : graph1(g1), graph2(g2), flag(flag) {}
+
 }
