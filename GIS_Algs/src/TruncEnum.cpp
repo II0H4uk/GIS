ecf9d0113b940ffee38a0d179ad7c23e3ddff1d5
II0H4uk <urmaer72@gmail.com>
2025-04-09 01:35:20 +0300

    refact trunc enum

--- GIS_Algs/src/TruncEnum.cpp
+++ GIS_Algs/src/TruncEnum.cpp
@@ -33,30 +33,46 @@ namespace GIS_Algs {
         for (int i = 0; i < g2.GetNodeCount(); ++i) {
             if (used[i]) continue;
 
-
             if (g1.GetAdjList()[depth].size() != g2.GetAdjList()[i].size()) continue;
 
+            int retFlag;
+            CompareTags(g1, depth, g2, i, retFlag);
+            if (retFlag == 3) continue;
 
-            const auto& tags1 = g1.GetTag()[depth];
-            const auto& tags2 = g2.GetTag()[i];
-
-            if (tags1.size() != tags2.size()) continue;
-            bool tagsEqual = true;
-            for (size_t j = 0; j < tags1.size(); ++j) {
-                if (!GIS_Data::CompareTags(tags1[j], tags2[j])) {
-                    tagsEqual = false;
-                    break;
-                }
-            }
-            if (!tagsEqual) continue;
-
-            mapping[depth] = i;
-            used[i] = true;
-
-            if (CompareGraphs(g1, g2, mapping, used, depth + 1)) return true;
-
-            used[i] = false;
+            bool boolFlag;
+            bool retVal = TryToMatchNodes(mapping, depth, i, used, g1, g2, boolFlag);
+            if (boolFlag) return retVal;
         }
         return false;
+    }
+
+    bool TruncEnum::TryToMatchNodes(std::vector<int>& mapping, int depth, int i, std::vector<bool>& used, const GIS_Data::Graph& g1, const GIS_Data::Graph& g2, bool& retFlag)
+    {
+        retFlag = true;
+        mapping[depth] = i;
+        used[i] = true;
+
+        if (CompareGraphs(g1, g2, mapping, used, depth + 1)) return true;
+
+        used[i] = false;
+        retFlag = false;
+        return {};
+    }
+
+    void TruncEnum::CompareTags(const GIS_Data::Graph& g1, int depth, const GIS_Data::Graph& g2, int i, int& retFlag)
+    {
+        retFlag = 1;
+        const auto& tags1 = g1.GetTag()[depth];
+        const auto& tags2 = g2.GetTag()[i];
+
+        if (tags1.size() != tags2.size()) { retFlag = 3; return; };
+        bool tagsEqual = true;
+        for (size_t j = 0; j < tags1.size(); ++j) {
+            if (!GIS_Data::CompareTags(tags1[j], tags2[j])) {
+                tagsEqual = false;
+                break;
+            }
+        }
+        if (!tagsEqual) { retFlag = 3; return; };
     }
 }
--- GIS_Core/src/GIS_Core.cpp
+++ GIS_Core/src/GIS_Core.cpp
@@ -11,23 +11,25 @@ int main(int argc, char* argv[]) {
     GIS_Data::Config config = GIS_Parser::Config::Parse("../config.ini");
 
     GIS_Generator::Generator genertor;
-    GIS_Data::GraphPair pair = genertor.GenerateGraphPair(10, true);
+    GIS_Data::GraphPair pair = genertor.GenerateGraphPair(8, true);
 
     /*GIS_Data::Graph graph1 = GIS_Parser::Graph::Parse(config.GetInput1());
     GIS_Data::Graph graph2 = GIS_Parser::Graph::Parse(config.GetInput2());*/
 
-    auto start = std::chrono::high_resolution_clock::now();
-    //bool result = GIS_Algs::TruncEnum::Start(pair.GetGraph1(), pair.GetGraph2());
-    bool result = GIS_Algs::BruteForce::Start(pair.GetGraph1(), pair.GetGraph2());
-    auto end = std::chrono::high_resolution_clock::now();
-    
-    std::chrono::duration<double> duration = end - start;
+    auto start1 = std::chrono::high_resolution_clock::now();
+    bool result1 = GIS_Algs::BruteForce::Start(pair.GetGraph1(), pair.GetGraph2());
+    auto end1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration1 = end1 - start1;
 
-    GIS_Stats::Statistics::Save(duration.count(), result, pair.GetGraph1().GetNodeCount(), config.GetOutput());
-    std::cout << duration;
+    std::cout << "Brute force end" << std::endl;
 
-    
-    
+    auto start2 = std::chrono::high_resolution_clock::now();
+    bool result2 = GIS_Algs::TruncEnum::Start(pair.GetGraph1(), pair.GetGraph2());
+    auto end2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration2 = end2 - start2;
+
+    GIS_Stats::Statistics::Save(duration1.count(), result1, pair.GetGraph1().GetNodeCount(), config.GetOutput());
+    GIS_Stats::Statistics::Save(duration2.count(), result2, pair.GetGraph1().GetNodeCount(), config.GetOutput());
 
     return 0;
 }
